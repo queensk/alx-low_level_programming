a585c38b62c135bc01f552f8c87a5046a4de5bcd
queensk <queenskisivuli@gmail.com>
2022-07-11 13:22:34 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holbaton.h"
+
+/**
+ *reste_to_98 takes a pointer to an int as perameter
+ * and updates the value to  point to 98
+ * returns void
+ */
+
+void reset_to_98(int *n)
+{
+  *n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+poiners allow performance of tasks such as direct memory alocation efficiently. A pointer is a variable whose value is the address of another variable.
+
+it is decleared as type *var-name;
