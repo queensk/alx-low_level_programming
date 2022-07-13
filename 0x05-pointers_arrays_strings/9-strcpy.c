ad77e679261ac0a87dc76db759ae27a9a2e784ab
queensk <queenskisivuli@gmail.com>
2022-07-12 20:42:21 -0700

    a function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+/**
+ *_strcpy - copies a string pointed to be src,
+ *inlcuding the \0 charcher of the buffer
+ *@dest: the copied string
+ *@src: the pointe to the string to be copied.
+ *
+ *Description:  a function that copies the string pointed to by src
+ *Return: string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+  int len = 0, i;
+  for (; src[len] != '\0'; len++)
+    ;
+  for (i = 0; i<len; i++)
+    dest[i] = src[i];
+  dest[i] = '\0';
+
+  return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void print_array(int *a, int n);
 void puts_half(char *str);
