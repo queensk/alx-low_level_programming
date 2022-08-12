21561df053023574f8828e28410dcf17c4a6f2a1
queensk <queenskisivuli@gmail.com>
2022-08-12 12:12:12 -0700

    fixed the betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,24 @@
 #include "lists.h"
 /**
+ * print_list - prints all the elements of a list.
+ * @h: single linked list to be printed.
+ *
+ * Description: The while loop checks if the list is null,
+ * if the head string points to null print its content,
+ * else it prints out the content of the list.
+ * Return: number of elements in the list.
  */
 
 size_t print_list(const list_t *h)
 {
-  size_t school_list;
-  school_list = 0;
-  while (h != NULL){
-    if (h->str == NULL)
-      {
-	if (h->str == NULL)
+          size_t school_list;
+          school_list = 0;
+          while (h != NULL)
 	  {
-	    printf("[%d] %s\n", 0, "(nil)");
+	          if (h->str == NULL)
+        	  printf("[%d] %s\n", 0, "(nil)");
 	    else
-	      printg("[%d] %s\n", h->len, h->str);
+	          printg("[%d] %s\n", h->len, h->str);
 	    h = h->next;
 	    school_list++;
 	  }
