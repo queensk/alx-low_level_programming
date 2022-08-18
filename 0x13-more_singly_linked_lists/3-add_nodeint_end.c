6bc2229ac5e355fca259a2307f963a7fd7e86dce
queensk <queenskisivuli@gmail.com>
2022-08-17 17:36:17 -0700

    fixed the error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
         start = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	  return (NULL);
+	        return (NULL);
 	new -> n = n;
 	new -> next = NULL;
 	if (*head == NULL);
@@ -29,6 +29,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 	        start = start -> next;
 	}
+	
 	start -> next = new;
 	return (new);
 }
