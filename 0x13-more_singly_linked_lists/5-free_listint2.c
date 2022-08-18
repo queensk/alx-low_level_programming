0536b347ee15bcaf2e4d4193b0549327df2f50f5
queensk <queenskisivuli@gmail.com>
2022-08-17 19:06:29 -0700

    fixed error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,21 @@
 #include "lists.h"
+
 /**
- * free_listint2 - free a list of its data.
- * @head: pointer to list
- *
- * Description: while header is persent,
- * clear its data
- * Return: void
+ * free_listint2 - free list
+ * @head:pointer to list
+ * Return:void
  */
 void free_listint2(listint_t **head)
 {
-        listint_t *c;
+	listint_t *c;
 
-        if (head == NULL);
-	        return;
-	while (*head)
-       	{
-	        c = *head;
-                *head = (*head)->next;
-                free(c);
-	}
-        head = NULL;
+if (head == NULL)
+	return;
+while (*head)
+{
+	c = *head;
+	*head = (*head)->next;
+	free(c);
+}
+head = NULL;
 }
