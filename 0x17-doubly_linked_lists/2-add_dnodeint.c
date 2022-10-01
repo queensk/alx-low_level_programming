c85cc752f30e3baa5fdc4d76f7c9d761eb4a2e43
queensk <queenskisiculi@gmail.com>
2022-10-01 09:33:43 +0000

    fixed error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,38 +1,37 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - add a new node at the beginning
- * of liked list.
- * 
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
+ *
  * @head: head of the list
- * @n: value of element to be added.
- * Return: returns the address of a new node 
+ * @n: value of the element
+ * Return: the address of the new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-        dlistint_t *new;
+	dlistint_t *new;
 	dlistint_t *h;
-	
 
 	new = malloc(sizeof(dlistint_t));
-	if (new = NULL)
-                return (NULL);
-	
+	if (new == NULL)
+		return (NULL);
+
 	new->n = n;
 	new->prev = NULL;
 	h = *head;
-	
+
 	if (h != NULL)
 	{
-	        while (h->prev != NULL)
-		  h->prev = n;
+		while (h->prev != NULL)
+			h = h->prev;
 	}
 
 	new->next = h;
-	
+
 	if (h != NULL)
-	        h->prev = new;
-	
+		h->prev = new;
+
 	*head = new;
 
 	return (new);
