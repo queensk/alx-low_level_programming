1329112be406572cd0f0ecdd93d9eb10a999d38f
queens kisivuli <queenskisivuli@gmail.com>
2022-10-27 11:32:51 +0300

    fixed betty style

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,14 +5,15 @@
  * @str: string to hash
  * Return: hashed value
  */
-unsigned long int hash_djb2(const unsigned char *str )
+unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hsh;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hsh = 5381;
-    while ((c = *str++))
-        hsh = ((hsh << 5) + hsh) + c;   /* hsh * 33 + c */
-    
-    return(hsh);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
