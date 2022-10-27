2fca6dbe370aef5f15ffc379f3ebb5a1a70008e1
queens kisivuli <queenskisivuli@gmail.com>
2022-10-27 14:11:06 +0300

    fixed betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,4 @@
-#include <hash_tables.h>
+#include "hash_tables.h"
 
 /**
  * hash_table_print - Print a hash table
@@ -6,30 +6,32 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *tmp;
-    unsigned long int i;
-    int toggle;
+	hash_node_t *tmp;
+	unsigned long int i;
+	int toggle;
 
-    if (ht == NULL)
-        return;
-    
-    printf("{");
-    for (i = 0, toggle = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        if (tmp != NULL)
-        {
-            if (toggle == 1)
-                printf(",");
-            printf("'%s': '%s'", tmp->key, tmp->value);
-            while ((tmp = tmp->next) != NULL)
-            {
-                printf(", ");
-                printf("'%s': '%s'", tmp->key, tmp->value);
-            }
+	if (ht == NULL)
+		return;
 
-            toggle = 1;
-        }
-    }
-    printf("}");
+	printf("{");
+
+	for (i = 0, toggle = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		if (tmp != NULL)
+		{
+			if (toggle == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			while ((tmp = tmp->next) != NULL)
+			{
+				printf(", ");
+				printf("'%s': '%s'", tmp->key, tmp->value);
+			}
+
+			toggle = 1;
+		}
+	}
+
+	printf("}\n");
 }
