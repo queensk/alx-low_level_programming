5765cdbcf89fdd1cadab95a6013e50458f0ecd56
queens kisivuli <queenskisivuli@gmail.com>
2022-10-27 13:35:53 +0300

    fixed betty style

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,23 +8,23 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *tmp;
-    unsigned long int idx;
+	hash_node_t *tmp;
+	unsigned long int idx;
 
-    if (ht == NULL || key == NULL)
-        return (NULL);
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-    idx = key_index((const unsigned char *)key, ht->size);
-    tmp = ht->array[idx];
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
 
-    if (tmp == NULL)
-        return (NULL);
-    
-    while (strcmp(tmp->key, key) != 0)
-        tmp = tmp->next;
+	if (tmp == NULL)
+		return (NULL);
 
-    if (tmp == NULL)
-        return (NULL);
-    
-    return (tmp->value);
+	while (strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+
+	if (tmp == NULL)
+		return (NULL);
+
+	return (tmp->value);
 }
